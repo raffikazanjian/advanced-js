653da4d19b1e8ac511d22815b76fe94dea43f163
Raffi Kazanjian <raffik@hotmail.com>
2016-05-09 17:22:52 -0700

    https://github.com/raffikazanjian/advanced-js.git

--- js/iterationPractice-2.js
+++ js/iterationPractice-2.js
@@ -1,14 +1,26 @@
 var bottles = " bottles of beer on the wall.";
-var beer = " bottles of beer."
-var takeOneDown = " You take one down, pass it around."
+var beer = " bottles of beer.";
+var takeOneDown = " You take one down, pass it around.";
 
-for (var i=99; i > 0; i--) {
+for (var i=99; i >= 0; i--) {
 	if(i > 1) {
 		console.log(i + bottles);
 		console.log(i + beer);
 		console.log(takeOneDown);
-		console.log(i-1 + bottles +'\n');
+		console.log(i-1 + bottles);
+		console.log(' ');
+	} else if (i === 1) {
+		console.log (i + ' bottle of beer on the wall.')
+		console.log(i + ' bottle of beer.')
+		console.log(takeOneDown);
+		console.log(i-1 + bottles);
+		console.log(' ');
+		alert('Ending 1 bottle of beer');
 	} else {
+		alert('Starting 0 bottles of beer');
+		console.log('No bottles of beer on the wall!');
+		console.log('No bottles of beer!');
 		console.log('You need to buy more beer!');
+		console.log(' ');
 	}
 }
